7700e40d15b693dd16f15e0f029f43042f2f16cd
DevannKirkptrick <55711679+DevannKirkptrick@users.noreply.github.com>
2019-11-07 15:17:31 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/17hCM35S2NNP7_EeOAR0l1Y1WtNonBhRhlCQJWNExibE/edit#gid=0';
